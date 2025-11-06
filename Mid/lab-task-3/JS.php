8a3366a87fee3b34c13642154e92e95b4f25dcab
Bikas-cloud <shilb677@gmail.com>
2025-11-06 14:40:43 +0600

    JS Practise

---
+++ Mid/lab-task-3/JS.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>JS lab class</title>
+</head>
+<body>
+    <h1>this is js lab 1st class</h1>
+
+    <script>
+        document.write("hello world");
+
+        console.log("hello world");
+
+        alert("hello world");
+
+        var name = "kaissha";
+        var age = 20;
+        var is_human = true;
+        var x;
+        console.log("type of the name : " + typeof(name));
+          console.log("type of the age : " + typeof (age));
+            console.log("type of the is_human : " + typeof(is_human));
+              console.log("type of the x : " + typeof(x));
+
+        var a = 10;
+        var b = 20;
+        var sum = a + b;
+        console.log("the sum is : " + sum);
+        console.log(`the difference is : ${a-b}`);
+        console.log(`the product is : ${a*b}`);
+        console.log(`the division is : ${a/b}`);
+        console.log(`the modulus is : ${a%b}`);
+
+              
+    </script>
+</body>
+</html>
